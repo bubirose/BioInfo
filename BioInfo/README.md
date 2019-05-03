7c984517f3ae46c2808a81d3505f7408652ef821
Ljubica <bubirose@gmail.com>
2019-05-03 15:46:57 +0200

    Initial setup and nav started

---
+++ BioInfo/README.md
@@ -0,0 +1,2 @@
+# BioInfo
+This is a project for MSc degree, on the Faculty of Mathematics.
---
+++ src/app/bio-info-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bio-info-app',
+  template: `
+  <h2>Hello world!</h2>
+  <img src="/assets/images/bio-info-logo-512.png"/>
+  `
+})
+export class BioInfoAppComponent {
+  title = 'bio-info';
+}
